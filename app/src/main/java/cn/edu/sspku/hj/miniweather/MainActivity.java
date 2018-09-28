2a5b9846d4b8e83a7a40c8c2ba7baa8059abd16c
plateauStar <runnerhuang@qq.com>
2018-09-28 18:05:54 +0800

    init project

---
+++ app/src/main/java/cn/edu/sspku/hj/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.sspku.hj.miniweather;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
+
+
