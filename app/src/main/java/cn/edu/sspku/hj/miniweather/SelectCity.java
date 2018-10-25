6793cff854c72cdd93cb58273614df26239fd574
runnerhuang@qq.com <runnerhuang@qq.com>
2018-10-25 20:33:53 +0800

    SearchView implemented

--- app/src/main/java/cn/edu/sspku/hj/miniweather/SelectCity.java
+++ app/src/main/java/cn/edu/sspku/hj/miniweather/SelectCity.java
@@ -18,10 +18,13 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cn.edu.sspku.hj.app.MyApplication;
 import cn.edu.sspku.hj.bean.City;
+import cn.edu.sspku.hj.util.PinYin;
 
 
 public class SelectCity extends Activity implements View.OnClickListener {
@@ -32,8 +35,10 @@ public class SelectCity extends Activity implements View.OnClickListener {
     private SearchView searchView;
 
     private MyApplication myApplication;
-    private ArrayList<String> mcityCodeAndNameList = new ArrayList<>();
-    private ArrayList<String> mSearchResult;
+
+    private ArrayList<String> mSearchResult = new ArrayList<>(); //搜索结果，只放城市名
+    private Map<String,String> nameToCode = new HashMap<>();  //城市名到编码
+    private Map<String,String> nameToPinyin = new HashMap<>(); //城市名到拼音
 
     private ArrayAdapter<String> adapter;
 
@@ -47,46 +52,49 @@ public class SelectCity extends Activity implements View.OnClickListener {
         mbackBtn = (ImageView) findViewById(R.id.title_back);
         mbackBtn.setOnClickListener(this);
         mcitySelect = (TextView) findViewById(R.id.title_name);
-        mlistView = (ListView) findViewById(R.id.city_list);
 
+        mlistView = (ListView) findViewById(R.id.city_list);
         mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(SelectCity.this, "你选择" + mSearchResult.get(position), Toast.LENGTH_SHORT).show();
-                returnCode = mSearchResult.get(position).substring(0, 9);
+                String returnCityName = mSearchResult.get(position);
+                Toast.makeText(SelectCity.this, "你选择" + returnCityName,
+                        Toast.LENGTH_SHORT).show();
+                returnCode = nameToCode.get(returnCityName); //通过城市名Key，获得城市编码Value
                 Log.d("Msea", returnCode);
-                mcitySelect.setText("当前城市：" +
-                        mSearchResult.get(position).substring(9));
+                mcitySelect.setText("当前城市：" +  //更新TextView
+                        returnCityName);
             }
         });
 
-        searchView = (SearchView) findViewById(R.id.search);
-        searchView.setIconified(true);
-        searchView.setQueryHint("请输入城市名称");
 
+
+        searchView = (SearchView) findViewById(R.id.search);
+        searchView.setIconified(true); //需要点击搜索图标，才展开搜索框
+        searchView.setQueryHint("请输入城市名称或拼音");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                //实际不搜索，文本框一变化就自动执行搜索
-                Toast.makeText(SelectCity.this, "检索中", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) { //newText先搜汉字的，后面升级拼音模糊搜索
-                if (!TextUtils.isEmpty(newText)) {
-                    Toast.makeText(SelectCity.this, "开始执行搜索", Toast.LENGTH_SHORT).show();
-                    if (mSearchResult != null)
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)) { //搜索栏不空时，执行搜索
+                    if (mSearchResult != null) //清空上次搜索结果
                         mSearchResult.clear();
-                    for (String str : mcityCodeAndNameList) {
-                        if (str.contains(newText)) {
+                    //遍历nameToPinyin的键值（它包含所有城市名），如果HashMap中的当前城市包含当前搜索框文字，
+                    //或拼音包含当前搜索框拼音，则认为该城市符合要求，放入mSearchResult
+                    for (String str : nameToPinyin.keySet()) {
+                        if (str.contains(newText)||nameToPinyin.get(str).contains(newText)) {
                             mSearchResult.add(str);
                         }
                     }
                     adapter.notifyDataSetChanged();
                 }
-                //后期改filter实现？实现filterable接口
-                return true; //return true 和 false 有什么区别？
+
+                return true;
+            }
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //实际不执行，文本框一变化就自动执行搜索
+                Toast.makeText(SelectCity.this, "检索中", Toast.LENGTH_SHORT).show();
+                return true;
             }
         });
 
@@ -97,10 +105,18 @@ public class SelectCity extends Activity implements View.OnClickListener {
     protected void initView() {
         myApplication = MyApplication.getInstance();
         ArrayList<City> mCityList = (ArrayList<City>) myApplication.getCityList();
+        String strName;
+        String strNamePinyin;
+        String strCode;
         for (City city : mCityList) {
-            mcityCodeAndNameList.add(city.getNumber() + city.getCity());
+            strCode = city.getNumber();
+            strName = city.getCity();
+            strNamePinyin = PinYin.converterToSpell(strName); //城市名解析成拼音
+            nameToCode.put(strName,strCode); //城市名到城市编码
+            nameToPinyin.put(strName,strNamePinyin); //城市名到拼音
+            mSearchResult.add(strName); //初始状态包含全部城市
+
         }
-        mSearchResult = new ArrayList<>(mcityCodeAndNameList); //浅拷贝
         adapter = new ArrayAdapter<> //新建适配器
                 (SelectCity.this, android.R.layout.simple_list_item_1, mSearchResult);
         mlistView.setAdapter(adapter); //接上适配器
